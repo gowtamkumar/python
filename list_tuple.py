4cdd563c0019d24944d88d819813429190a9702e
gowtamkumar <gowtampaul0@gmail.com>
2024-04-04 12:17:31 +0600

    list and tuple

---
+++ list_tuple.py
@@ -0,0 +1,39 @@
+## list and tuple
+
+# list is as like js ar moto
+
+marks = [33.4, 55, 99, 40, 39,33]
+
+# marks[0] = 100 ## new add
+# marks[1] = 88 ## upate data as like js ar moto
+# del marks[1] ## delete data
+# slicing
+
+# print(marks[2:3])
+
+# print(marks[2]) ## access
+# print(len(marks)) ## len
+# print(marks[0])
+# newappend = marks.append(77)
+# print(marks) ## appened
+# newsort = marks.sort() ##sort
+# print(marks)
+
+# newRevered = marks.sort(reverse=True) ## reverse
+# newRevered = marks.reverse() ## reverse
+# print(marks)
+# marks.insert(0, 44) ## you can  index onujay data insert kora
+# print(marks)
+
+# marks.remove(33) ## remove fun working find value then remove
+# print(marks)
+# marks.pop(0) ## this pop working index by remove for list
+# print(marks)
+
+## tuple 
+  # tuple you can access but you can not change delete as like string
+
+tup = (33.4, 55, 99, 40, 39,33)
+print(tup[2])
+print(tup.index(55)) ## index only find element
+print(tup.count(55)) ## count only how many exisit the value
