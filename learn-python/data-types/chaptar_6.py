4a39f5624aca93122785e1d04dbff94ec0021278
gowtamkumar <gowtampaul0@gmail.com>
2025-10-14 21:53:27 +0600

    string

---
+++ learn-python/data-types/chaptar_6.py
@@ -0,0 +1,12 @@
+chai_type = "Gp Chai"
+customer_name = "Arko"
+
+print(f"order for {customer_name} : {chai_type} please!")
+
+chai_description = "Aromatic and Bold"
+
+print(f"lenth : {len(chai_description)}")
+print(f"First word: {chai_description[:8]}")
+print(f"Last word: {chai_description[12:]}")
+print(f"last word: {chai_description[::-1]}") 
+# reversed
