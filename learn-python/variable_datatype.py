730f24bd16e70ac8913d6b5cc390886077496fd6
gowtamkumar <gowtampaul0@gmail.com>
2025-10-22 08:15:05 +0600

    variable and datatype

---
+++ learn-python/variable_datatype.py
@@ -0,0 +1,32 @@
+# str -> String (text)
+name = "gowtam kumar"
+# int -> Integer (whole number)
+age = 34
+# float -> Floating point number (decimal)
+height = 5.9
+# bool -> Boolean (True/False)
+is_student = False
+# list -> List (ordered collection) Ordered, changeable list
+fruits = ["apple", "banana", "cherry"]
+# dict -> Dictionary (key-value pairs)
+person = {"name": "Alice", "age": 30}
+# tuple -> Tuple (ordered, immutable collection) Ordered, unchangeable list
+point = (10, 20, 400)
+# set -> Set (unordered collection of unique items)
+colors = {"red", "green"}
+
+
+# Different datatypes
+name = "Gowtam"  # str
+age = 25  # int
+height = 5.9  # float
+is_developer = True  # bool
+skills = ["Python", "JavaScript"]  # list
+location = ("BD", "Dhaka")  # tuple
+info = {"email": "gowtam@email.com"}  # dict
+unique_ids = {1, 2, 3}  # set
+nothing = None  # NoneType
+
+# Printing
+print(name, age, height, is_developer)
+print(type(skills))  # <class 'list'>
