c75b2a9372f716778855ef768f361eb12653802c
gowtamkumar <gowtampaul0@gmail.com>
2026-08-25 20:29:15 +0600

    Add Conditions.py with voting eligibility and grading logic

---
+++ Conditions.py
@@ -0,0 +1,18 @@
+age = 20
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("you are not eligible to vote.")
+
+score = 75
+
+if score >= 80:
+    print("You got an A+ grade.")
+elif score >= 70:
+    print("You got a A Grade.")
+elif score >= 60:
+    print("You got a B Grade.")
+elif score >= 50:
+    print("You got a C Grade.")
+else:
+    print("You got a D Grade.")
